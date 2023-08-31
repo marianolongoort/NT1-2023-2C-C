8f2188bb3308fc7b28ef055f53cf5bb6dc72e84d
marianolongoort <mariano.longo@ort.edu.ar>
2023-08-30 21:13:59 -0300

    Carga de propiedades de ejemplo.

--- Models/Persona.cs
+++ Models/Persona.cs
@@ -3,5 +3,12 @@
     public class Persona
     {
         public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public Direccion Direccion { get; set; }
+
+        public List<Telefono> Telefonos { get; set; }
+
     }
 }
