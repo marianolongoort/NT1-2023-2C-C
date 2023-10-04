228f45cb1a4efe8b3313443ada00c7d1baff6e34
marianolongoort <mariano.longo@ort.edu.ar>
2023-10-04 18:55:00 -0300

    Add.BasicScaffolding

---
+++ Controllers/ClientesVehiculosController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Estacionamiento_C.Data;
+using Estacionamiento_C.Models;
+
+namespace Estacionamiento_C.Controllers
+{
+    public class ClientesVehiculosController : Controller
+    {
+        private readonly MiDbContext _context;
+
+        public ClientesVehiculosController(MiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ClientesVehiculos
+        public async Task<IActionResult> Index()
+        {
+            var miDbContext = _context.ClientesVehiculos.Include(c => c.Cliente).Include(c => c.Vehiculo);
+            return View(await miDbContext.ToListAsync());
+        }
+
+        // GET: ClientesVehiculos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.ClientesVehiculos == null)
+            {
+                return NotFound();
+            }
+
+            var clienteVehiculo = await _context.ClientesVehiculos
+                .Include(c => c.Cliente)
+                .Include(c => c.Vehiculo)
+                .FirstOrDefaultAsync(m => m.ClienteId == id);
+            if (clienteVehiculo == null)
+            {
+                return NotFound();
+            }
+
+            return View(clienteVehiculo);
+        }
+
+        // GET: ClientesVehiculos/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Apellido");
+            ViewData["VehiculoId"] = new SelectList(_context.Vehiculos, "Id", "Id");
+            return View();
+        }
+
+        // POST: ClientesVehiculos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ClienteId,VehiculoId")] ClienteVehiculo clienteVehiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(clienteVehiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Apellido", clienteVehiculo.ClienteId);
+            ViewData["VehiculoId"] = new SelectList(_context.Vehiculos, "Id", "Id", clienteVehiculo.VehiculoId);
+            return View(clienteVehiculo);
+        }
+
+        // GET: ClientesVehiculos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.ClientesVehiculos == null)
+            {
+                return NotFound();
+            }
+
+            var clienteVehiculo = await _context.ClientesVehiculos.FindAsync(id);
+            if (clienteVehiculo == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Apellido", clienteVehiculo.ClienteId);
+            ViewData["VehiculoId"] = new SelectList(_context.Vehiculos, "Id", "Id", clienteVehiculo.VehiculoId);
+            return View(clienteVehiculo);
+        }
+
+        // POST: ClientesVehiculos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ClienteId,VehiculoId")] ClienteVehiculo clienteVehiculo)
+        {
+            if (id != clienteVehiculo.ClienteId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(clienteVehiculo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClienteVehiculoExists(clienteVehiculo.ClienteId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Apellido", clienteVehiculo.ClienteId);
+            ViewData["VehiculoId"] = new SelectList(_context.Vehiculos, "Id", "Id", clienteVehiculo.VehiculoId);
+            return View(clienteVehiculo);
+        }
+
+        // GET: ClientesVehiculos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.ClientesVehiculos == null)
+            {
+                return NotFound();
+            }
+
+            var clienteVehiculo = await _context.ClientesVehiculos
+                .Include(c => c.Cliente)
+                .Include(c => c.Vehiculo)
+                .FirstOrDefaultAsync(m => m.ClienteId == id);
+            if (clienteVehiculo == null)
+            {
+                return NotFound();
+            }
+
+            return View(clienteVehiculo);
+        }
+
+        // POST: ClientesVehiculos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.ClientesVehiculos == null)
+            {
+                return Problem("Entity set 'MiDbContext.ClientesVehiculos'  is null.");
+            }
+            var clienteVehiculo = await _context.ClientesVehiculos.FindAsync(id);
+            if (clienteVehiculo != null)
+            {
+                _context.ClientesVehiculos.Remove(clienteVehiculo);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClienteVehiculoExists(int id)
+        {
+          return _context.ClientesVehiculos.Any(e => e.ClienteId == id);
+        }
+    }
+}
---
+++ Controllers/EstanciasController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Estacionamiento_C.Data;
+using Estacionamiento_C.Models;
+
+namespace Estacionamiento_C.Controllers
+{
+    public class EstanciasController : Controller
+    {
+        private readonly MiDbContext _context;
+
+        public EstanciasController(MiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Estancias
+        public async Task<IActionResult> Index()
+        {
+            var miDbContext = _context.Estancias.Include(e => e.Cliente).Include(e => e.Vehiculo);
+            return View(await miDbContext.ToListAsync());
+        }
+
+        // GET: Estancias/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Estancias == null)
+            {
+                return NotFound();
+            }
+
+            var estancia = await _context.Estancias
+                .Include(e => e.Cliente)
+                .Include(e => e.Vehiculo)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (estancia == null)
+            {
+                return NotFound();
+            }
+
+            return View(estancia);
+        }
+
+        // GET: Estancias/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Apellido");
+            ViewData["VehiculoId"] = new SelectList(_context.Vehiculos, "Id", "Id");
+            return View();
+        }
+
+        // POST: Estancias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,VehiculoId,ClienteId,Monto,Inicio,Fin")] Estancia estancia)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(estancia);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Apellido", estancia.ClienteId);
+            ViewData["VehiculoId"] = new SelectList(_context.Vehiculos, "Id", "Id", estancia.VehiculoId);
+            return View(estancia);
+        }
+
+        // GET: Estancias/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Estancias == null)
+            {
+                return NotFound();
+            }
+
+            var estancia = await _context.Estancias.FindAsync(id);
+            if (estancia == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Apellido", estancia.ClienteId);
+            ViewData["VehiculoId"] = new SelectList(_context.Vehiculos, "Id", "Id", estancia.VehiculoId);
+            return View(estancia);
+        }
+
+        // POST: Estancias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,VehiculoId,ClienteId,Monto,Inicio,Fin")] Estancia estancia)
+        {
+            if (id != estancia.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(estancia);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EstanciaExists(estancia.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Apellido", estancia.ClienteId);
+            ViewData["VehiculoId"] = new SelectList(_context.Vehiculos, "Id", "Id", estancia.VehiculoId);
+            return View(estancia);
+        }
+
+        // GET: Estancias/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Estancias == null)
+            {
+                return NotFound();
+            }
+
+            var estancia = await _context.Estancias
+                .Include(e => e.Cliente)
+                .Include(e => e.Vehiculo)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (estancia == null)
+            {
+                return NotFound();
+            }
+
+            return View(estancia);
+        }
+
+        // POST: Estancias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Estancias == null)
+            {
+                return Problem("Entity set 'MiDbContext.Estancias'  is null.");
+            }
+            var estancia = await _context.Estancias.FindAsync(id);
+            if (estancia != null)
+            {
+                _context.Estancias.Remove(estancia);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EstanciaExists(int id)
+        {
+          return _context.Estancias.Any(e => e.Id == id);
+        }
+    }
+}
